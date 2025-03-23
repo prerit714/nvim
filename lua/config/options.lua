c5c16e19fc504ee035a0555deb2dcf4f43d491e0
Prerit Dayal <prerit714@gmail.com>
2025-03-23 22:26:21 +0530

    Set render size of a single tab char

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,9 +2,20 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- Limit for a code block
 vim.opt.colorcolumn = "80"
+
+-- Turn off relative numbers
 vim.opt.relativenumber = false
+
 vim.g.snacks_animate = false
+
 vim.opt.wrap = false
+
+-- Invisible chars show
 vim.opt.list = true
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Render a tab 4 chars wide
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4

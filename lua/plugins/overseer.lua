064ff14bb9e15768d73a29b24b4d3b2d207670f7
Prerit Dayal <prerit714@gmail.com>
2025-05-18 23:55:50 +0530

    Init
    steps to clone
    
    Minor update to the clone command
    
    Show seconds on statusline + toggle wrap
    
    Another important keybinds
    
    A nil check + Trouble at bottom
    
    Show mode on statusline
    
    Needed keybinds
    
    Show hidden files + virtual lines support
    
    Turn off virtual text
    
    Removed toggle to autoformat - it did not work
    
    Got conform working with hotkey toggle
    
    Using kanagawabones!
    
    Darken comments
    
    Using a different key for local-leader
    
    Added a template for CP
    
    Back to strict templates for cpp
    
    I just like explicitness when it is low level stuff

---
+++ lua/plugins/overseer.lua
@@ -0,0 +1,32 @@
+return {
+  "stevearc/overseer.nvim",
+  opts = {},
+  config = function()
+    require("overseer").setup {
+      strategy = "toggleterm",
+      templates = { "builtin", "user.cpp_build", "user.cpp_run" },
+    }
+
+    -- Keybindings using localleader
+    local keymap = vim.keymap.set
+
+    -- Main keybindings
+    keymap("n", "<localleader>r", "<cmd>OverseerRun<cr>", { desc = "Run task" })
+    keymap("n", "<localleader>t", "<cmd>OverseerToggle<cr>", { desc = "Toggle task list" })
+    keymap("n", "<localleader>a", "<cmd>OverseerTaskAction<cr>", { desc = "Task action" })
+    keymap("n", "<localleader>b", "<cmd>OverseerBuild<cr>", { desc = "Build task" })
+    keymap("n", "<localleader>q", "<cmd>OverseerQuickAction<cr>", { desc = "Quick action" })
+    keymap("n", "<localleader>c", "<cmd>OverseerClearCache<cr>", { desc = "Clear cache" })
+
+    -- Additional useful keybindings
+    keymap("n", "<localleader>n", "<cmd>OverseerRunCmd<cr>", { desc = "Run shell command" })
+    keymap("n", "<localleader>l", "<cmd>OverseerLoadBundle<cr>", { desc = "Load task bundle" })
+    keymap("n", "<localleader>s", "<cmd>OverseerSaveBundle<cr>", { desc = "Save task bundle" })
+  end,
+  dependencies = {
+    {
+      "akinsho/toggleterm.nvim",
+      name = "toggleterm",
+    },
+  },
+}

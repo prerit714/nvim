93d776e8e8978b200e4f1bab18ab54fc1652a5b6
Prerit Dayal <prerit714@gmail.com>
2026-01-15 06:00:19 -0500

    cp.lua setup for myself

--- lua/plugins/cp.lua
+++ lua/plugins/cp.lua
@@ -1,5 +1,18 @@
+local developing_locally = false
+
+if developing_locally then
+  return {
+    dir = "~/Projects/Lua/cp.nvim",
+    name = "cp",
+    lazy = false,
+    config = function()
+      require("cp").setup()
+    end
+  }
+end
+
 return {
-  dir = "~/Projects/Lua/cp.nvim",
+  "prerit714/cp.nvim",
   name = "cp",
   lazy = false,
   config = function()

837f7997da5d92a1b36d0595f73422d5ac59fa0e
Prerit Dayal <prerit714@gmail.com>
2026-03-17 15:47:00 +0530

    neovide based settings

--- init.lua
+++ init.lua
@@ -1,3 +1,24 @@
+if vim.g.neovide then
+  vim.g.neovide_scale_factor = 1.0
+
+  local function change_scale(delta)
+    vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
+  end
+
+  -- Ctrl+= zoom in, Ctrl+- zoom out, Ctrl+0 reset
+  vim.keymap.set({ "n", "v", "i" }, "<C-=>", function() change_scale(1.1) end)
+  vim.keymap.set(
+    { "n", "v", "i" },
+    "<C-->",
+    function() change_scale(1 / 1.1) end
+  )
+  vim.keymap.set(
+    { "n", "v", "i" },
+    "<C-0>",
+    function() vim.g.neovide_scale_factor = 1.0 end
+  )
+end
+
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 vim.g.have_nerd_font = true

b79f0f56bdebd806741daa7093bec4413e944e4d
Prerit Dayal <prerit714@gmail.com>
2026-03-20 02:34:48 +0530

    chore: better startify

--- lua/custom/plugins/startify.lua
+++ lua/custom/plugins/startify.lua
@@ -2,4 +2,19 @@
 ---@type LazySpec
 return {
   "mhinz/vim-startify",
+  config = function()
+    vim.g.startify_lists = {
+      { type = "files", header = { "   Files" } },
+      {
+        type = "dir",
+        header = { "   Current Directory " .. vim.fn.getcwd() },
+      },
+      { type = "sessions", header = { "   Sessions" } },
+      { type = "bookmarks", header = { "   Bookmarks" } },
+    }
+
+    vim.g.startify_bookmarks = {
+      { i = "~/Projects" },
+    }
+  end,
 }

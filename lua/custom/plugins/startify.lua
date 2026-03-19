40ab9a6bce036bf109270c75324f09e8e47d93d5
Prerit Dayal <prerit714@gmail.com>
2026-03-20 02:40:02 +0530

    chore: more settings for startify

--- lua/custom/plugins/startify.lua
+++ lua/custom/plugins/startify.lua
@@ -16,5 +16,11 @@ return {
     vim.g.startify_bookmarks = {
       { i = "~/Projects" },
     }
+
+    vim.g.startify_session_autoload = 1
+    vim.g.startify_session_delete_buffers = 1
+    vim.g.startify_change_to_vcs_root = 1
+    vim.g.startify_fortune_use_unicode = 1
+    vim.g.startify_session_persistence = 1
   end,
 }

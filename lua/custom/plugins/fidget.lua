c80096c8eb3d2f959e93679e6833f9f1ac5a1ae7
Prerit Dayal <prerit714@gmail.com>
2026-04-02 22:18:10 +0530

    chore: added fidget

---
+++ lua/custom/plugins/fidget.lua
@@ -0,0 +1,9 @@
+---@module 'lazy'
+---@type LazySpec
+return {
+  "j-hui/fidget.nvim",
+  version = "*", -- alternatively, pin this to a specific version, e.g., "1.6.1"
+  opts = {
+    -- options
+  },
+}
